f17946d8e095bdda246697e1e70ecc6ad423d847
Jils Patel <jilspatel21@gmail.com>
2025-02-19 23:14:49 +0530

    Changes in the desgin

--- src/components/FeatureCards.jsx
+++ src/components/FeatureCards.jsx
@@ -2,17 +2,23 @@ export function FeatureCards(){
     return (
         <div className="flex justify-evenly mt-36">
             <div className="flex flex-col  items-center text-center max-w-72">
-                <img className="w-24 h-16  rounded-full mx-auto" src="../../images/img-1.png" alt="logo"></img>
+                <div className="w-20 h-20 bg-[#F4E7FE] rounded-full flex items-center justify-center overflow-hidden mx-auto">
+                    <img className="w-12 h-12 object-contain" src="../../images/target.svg" alt="logo"/>
+                </div>
                 <div className="flex justify-center text-lg font-semibold mt-2">Track Progress</div>
                 <p className="text-sm break-words mt-2 max-w-72 text-center">Monitor your learning journey with detailed analytics and progress tracking.</p>
             </div>
             <div className="flex flex-col">
-                <img className="w-25 h-16  rounded-full mx-auto" src="../../images/img-3.png" alt="logo"></img>
+                <div className="w-20 h-20 bg-[#F4E7FE] rounded-full flex items-center justify-center overflow-hidden mx-auto">
+                    <img className="w-12 h-12 object-contain" src="../../images/book.svg" alt="logo"/>
+                </div>
                 <div className="flex justify-center text-lg font-semibold mt-2">Track Progress</div>
                 <p className="text-sm break-words mt-2 max-w-72 text-center">Monitor your learning journey with detailed analytics and progress tracking.</p>
             </div>
             <div className="flex flex-col">
-                <img className="w-25 h-16  rounded-full mx-auto" src="../../images/img-2.png" alt="logo"></img>
+                <div className="w-20 h-20 bg-[#F4E7FE] rounded-full flex items-center justify-center overflow-hidden mx-auto">
+                    <img className="w-12 h-12 object-contain" src="../../images/trophy.svg" alt="logo"/>
+                </div>
                 <div className="flex justify-center text-lg font-semibold mt-2">Track Progress</div>
                 <p className="text-sm break-words mt-2 max-w-72 text-center">Monitor your learning journey with detailed analytics and progress tracking.</p>
             </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='w-10 h-full'>
+      
+    </div>
+  )
+}
+
+export default Sidebar
