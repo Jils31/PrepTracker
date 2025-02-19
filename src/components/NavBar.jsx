1e9f563528d9afcfb4e21aaeec7a52a53c752ad2
Jils Patel <jilspatel21@gmail.com>
2025-02-19 10:02:04 +0530

    Changes in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 export function NavBar() {
     return (
-        <nav className="w-full bg-white py-4 px-6 flex justify-between items-center">
+        <nav className="w-full bg-white py-4 px-6 flex justify-around items-center">
             <h1 className="text-xl text-violet-700 font-bold">PrepTracker</h1>
             <button className="bg-violet-700 text-white py-2 px-4 rounded-lg hover:bg-violet-800">
                 Sign in with Google
